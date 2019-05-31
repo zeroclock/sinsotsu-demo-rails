4b2b6c77fae46c502957ab84635546224d5b631d
EC2 Default User <ec2-user@ip-172-31-39-52.us-east-2.compute.internal>
2019-05-31 08:10:56 +0000

    ページタイトル修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # タイトル生成
+    def full_title(page_title='')
+      base_title = "講義用デモサイト"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+    end
 end
