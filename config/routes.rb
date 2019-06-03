a47b0d4d3c26e5792f9225c4ee5cd39860769e63
EC2 Default User <ec2-user@ip-172-31-39-52.us-east-2.compute.internal>
2019-06-03 04:05:57 +0000

    会員登録コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
   root 'static_pages#home'
+  
+  get '/signup', to: 'users#new'
+  get '/help', to: 'static_pages#help'
 end
