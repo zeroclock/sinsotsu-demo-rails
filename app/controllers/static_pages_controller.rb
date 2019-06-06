572e343bac8013355de64b60b4ec9f304b2bfee3
EC2 Default User <ec2-user@ip-172-31-39-52.us-east-2.compute.internal>
2019-06-06 08:49:50 +0000

    デザイン調整

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     @user = current_user
-    @posts = Post.paginate(page: params[:page], per_page: 10)
+    @posts = Post.paginate(page: params[:page], per_page: 5)
     @post = @user.posts.build if logged_in?
   end
 
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+    # ログインユーザーが投稿者と一致するか
+    def is_posted_user?(post)
+       current_user == post.user
+    end
 end
